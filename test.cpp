a0a55c031fa2981f0d7e77af1f0a22b0a5c0e7c1
Mandrathax <pmichel31415@gmail.Com>
2015-02-20 14:45:52 +0100

    trackbar test

--- test.cpp
+++ test.cpp
@@ -8,6 +8,30 @@
 using namespace cv;
 using namespace std;
 
+const int alpha_slider_max = 100;
+int alpha_slider;
+double alpha;
+double beta;
+
+/// Matrices to store images
+Mat src1;
+Mat src2;
+Mat dst;
+
+/**
+* @function on_trackbar
+* @brief Callback for trackbar
+*/
+void on_trackbar(int, void*)
+{
+	alpha = (double)alpha_slider / alpha_slider_max;
+	beta = (1.0 - alpha);
+
+	addWeighted(src1, alpha, src2, beta, 0.0, dst);
+
+	imshow("Linear Blend", dst);
+}
+
 int main()
 {
 	cout << "test" << endl;
