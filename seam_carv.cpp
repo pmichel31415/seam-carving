9fe04a2814a21762b15c33b1b149e0bb1a1a6e47
Antoine Prouvost <prouvost.antoine@gmail.com>
2015-02-20 14:48:02 +0100

    Image

--- seam_carv.cpp
+++ seam_carv.cpp
@@ -1,1 +1,6 @@
+#include "seam_carv.h"
 
+void function1(const Mat& I){
+    
+    
+}
--- seam_carv.h
+++ seam_carv.h
@@ -1,1 +1,15 @@
 
+#ifndef seam_carv_Header_h
+#define seam_carv_Header_h
+
+#include <iostream>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+
+using namespace cv;
+using namespace std;
+
+#endif
