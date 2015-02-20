d037873f25319e091f9f990f42b8261f246e83c0
Mandrathax <pmichel31415@gmail.Com>
2015-02-20 14:22:14 +0100

    1rst interesting commit

--- test.cpp
+++ test.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/nonfree/features2d.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 
+using namespace cv;
+using namespace std;
+
 int main()
 {
-	std::cout << "test" << std::endl;
+	cout << "test" << endl;
+	Mat A = imread("Broadway_tower_edit.jpg");
+	namedWindow("images");
+	imshow("images", A);	waitKey();
+	Mat I;
+	cvtColor(A, I, CV_RGB2GRAY);
+	namedWindow("images");
+	imshow("images", I);	waitKey();
 }
